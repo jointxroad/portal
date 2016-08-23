e76759788c9fb55d2ca4557ab88c0c10b2cc28a2
Karri Niemelä <kakoni@gmail.com>
2016-08-23 11:39:50 +0300

    adding modules

--- config.rb
+++ config.rb
@@ -26,6 +26,11 @@ activate :external_pipeline,
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
+data.modules.each_with_index do |component, index|
+  proxy "/module/#{index}.html", "module.html",
+    :locals => { :component => component },
+    :ignore => true
+end
 
 ###
 # Helpers
